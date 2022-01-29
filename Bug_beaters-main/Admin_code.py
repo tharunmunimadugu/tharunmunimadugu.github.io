78e0d5b09e99fbd4834a2ac1c48311185cc9c971
tharunmunimadugu <86178876+tharunmunimadugu@users.noreply.github.com>
2022-01-29 08:56:40 +0530

    webapplication

---
+++ Bug_beaters-main/Admin_code.py
@@ -0,0 +1,33 @@
+def check_login():
+	if :
+		print'valid user'
+	else 
+		print 'invalid user'
+
+def to_open_market():
+	market= True
+
+def to_close_market():
+	market= False
+	
+def check_placed_orders():
+	return placed_orders
+
+def execute_order(placed_orders):
+	if exec_price>=placed_price
+		split exec_qty
+			return True
+	else 
+		return False
+		
+	
+admin_id="9123"
+password="qwerty123"
+check_login()
+
+to_open_market()
+if market== True
+	check_placed_orders()
+to_close_market()
+if market== False
+	execute_order(placed_orders)
